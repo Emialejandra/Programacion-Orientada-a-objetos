2ce3ffada9e0bc0ba58e3987541ff1e8f4458b2e
Emialejandra <emilia.tana.p@gmail.com>
2025-11-12 20:04:30 -0500

    Tarea en grupo

--- TareaS6_Herencia/src/Ejemplos_Investigacion.java
+++ TareaS6_Herencia/src/Ejemplos_Investigacion.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Ejemplos_Investigacion {
+    static class Animal{
+        void comer(){
+            System.out.println("Animal comiendo");
+        }
+    }
+
+    static class Perro extends Animal{
+        void ladrar(){
+            System.out.println("Perro comiendo");
+        }
+    }
+
+    //metodo abstracto
+    //abstract void mover();
 }
--- TareaS6_Herencia/src/main.java
+++ TareaS6_Herencia/src/main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class main {
+    public static void main(String[] args) {
+        Ejemplos_Investigacion.Perro perrito=new Ejemplos_Investigacion.Perro();
+        perrito.comer();
+        perrito.ladrar();
+
+    }
 }
